1f0910b929d638538f5bcbbad8eb98027681bd10
linzi <linlin_zhao@yeah.net>
2018-11-21 20:24:34 +0800

    add git ignore

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import './App.css';
-import Header from './components/Header';
+import './App.scss';
 import Nav from './components/Nav';
 import WebWorker from './pages/webWorker/WebWorker';
 import WebSocket from './pages/webSocket/WebSocket';
@@ -9,8 +8,6 @@ import {BrowserRouter as Router,Route} from "react-router-dom";
 const App = () => (
     <Router>
         <div className="App">
-            <div>app</div>
-            <Header/>
             <Nav/>
             <Route exact={true} path="/" component={WebWorker}/>
             <Route path="/webWorker" component={WebWorker}/>
--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import './nav.css';
+import './nav.scss';
 import {Link} from "react-router-dom";
 
 class Nav extends React.PureComponent {
