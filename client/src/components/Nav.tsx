61e415a06e30974ab0d7e02c6c060c86bf16e35d
linzi <linlin_zhao@yeah.net>
2018-11-22 11:39:02 +0800

    add git ignore

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,24 +1,37 @@
 import * as React from 'react'
 import './nav.scss';
+import Divider from "@material-ui/core/Divider/Divider";
+import List from "@material-ui/core/List/List";
+import ListItem from "@material-ui/core/ListItem/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText/ListItemText";
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import {Link} from "react-router-dom";
 
-class Nav extends React.PureComponent {
+
+class Nav extends React.Component {
+    public state = {
+        selected:0
+    };
+    public clickSelect(selectedId: number):void{
+        this.setState({selected:selectedId})
+    }
     public render(){
         return (
         <div className='nav'>
-            <nav>
-                <ul>
-                    <li>
-                        <Link  to='/'>webworker</Link>
-                    </li>
-                    <li>
-                        <Link  to='/webWorker'>webworker</Link>
-                    </li>
-                    <li>
-                        <Link  to='/webSocket'>webworker</Link>
-                    </li>
-                </ul>
-            </nav>
+            <Divider />
+            <List>
+                {['webWorker', 'webSocket', 'Drafts'].map((text, index) => (
+                    <Link to={'/'+text} key={text}>
+                        <ListItem button={true}  selected={this.state.selected===index} onClick={ this.clickSelect.bind(this,index)}  >
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon/> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    </Link>
+                ))}
+            </List>
+            <Divider />
         </div>
         );
     }
