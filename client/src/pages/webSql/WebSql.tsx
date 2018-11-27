c831c24be89ef7ef621d55142bba1e51e8ad6c60
linzi <linzi4235@gmail.com>
2018-11-27 23:19:08 +0800

    表格

--- client/src/pages/webSql/WebSql.tsx
+++ client/src/pages/webSql/WebSql.tsx
@@ -1,6 +1,27 @@
 import * as React from 'react'
 import './webSql.scss';
 import WebSqlHelper from './WebSqlHelper';
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/es/Table";
+import TableHead from "@material-ui/core/es/TableHead";
+import TableRow from "@material-ui/core/es/TableRow";
+import TableCell from "@material-ui/core/es/TableCell";
+import TableBody from "@material-ui/core/es/TableBody";
+
+let id = 0;
+function createData(name:string, calories:number, fat:number, carbs:number, protein:number) {
+    id += 1;
+    return { id, name, calories, fat, carbs, protein };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 class WebSql extends React.PureComponent {
     public componentDidMount(){
@@ -10,7 +31,44 @@ class WebSql extends React.PureComponent {
     public render(){
         return (
         <div id='webSql'>
-            <p>websql</p>
+            <div className="operation">
+                <Button
+                    variant="contained"
+                    color='primary'
+                >
+                    create websql
+                </Button>
+            </div>
+
+
+            <Paper >
+                <Table >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell numeric={true}>Calories</TableCell>
+                            <TableCell numeric={true}>Fat (g)</TableCell>
+                            <TableCell numeric={true}>Carbs (g)</TableCell>
+                            <TableCell numeric={true}>Protein (g)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => {
+                            return (
+                                <TableRow key={row.id}>
+                                    <TableCell component="th" scope="row">
+                                        {row.name}
+                                    </TableCell>
+                                    <TableCell numeric={true}>{row.calories}</TableCell>
+                                    <TableCell numeric={true}>{row.fat}</TableCell>
+                                    <TableCell numeric={true}>{row.carbs}</TableCell>
+                                    <TableCell numeric={true}>{row.protein}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </Paper>
         </div>
         );
     }
